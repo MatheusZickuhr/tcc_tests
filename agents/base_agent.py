c2d0d6866f7688da35b6091fbaa6500420478f91
Matheus Zickuhr <mz97callofduty542@gmail.com>
2019-08-18 20:55:56 -0300

    adicionado venv ao gitignore

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -12,9 +12,9 @@ class Agent:
             virtual_joystick.go_right()
         elif action == agent_actions.GO_LEFT:
             virtual_joystick.go_left()
-        if action == agent_actions.GO_UP:
+        elif action == agent_actions.GO_UP:
             virtual_joystick.go_up()
-        if action == agent_actions.GO_DOWN:
+        elif action == agent_actions.GO_DOWN:
             virtual_joystick.go_down()
-        if action == agent_actions.DROP_BOMB:
+        elif action == agent_actions.DROP_BOMB:
             virtual_joystick.drop_bomb()
