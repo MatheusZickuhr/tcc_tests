8d6d54679ebda2a0d600021904fd0f16ebc28317
MatheusZickuhr <mz97callofduty542@gmail.com>
2020-08-24 08:44:17 -0300

    install script, requirements.txt & stats

---
+++ install.sh
@@ -0,0 +1,27 @@
+git clone https://github.com/MatheusZickuhr/PyGame-Learning-Environment.git
+cd PyGame-Learning-Environment
+pip install -e .
+cd ..
+
+git clone https://github.com/MatheusZickuhr/keras-rl.git
+cd keras-rl
+pip install -e .
+cd ..
+
+git clone https://github.com/MatheusZickuhr/neat-python.git
+cd neat-python
+pip install -e .
+cd ..
+
+git clone https://github.com/MatheusZickuhr/python-ne.git
+cd python-ne
+pip install -e .
+cd ..
+
+git clone https://github.com/MatheusZickuhr/neuroevolution-sandbox.git
+cd neuroevolution-sandbox
+pip install -e .
+cd ..
+
+pip install -r requirements.txt
+
